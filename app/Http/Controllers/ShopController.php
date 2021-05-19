eefc935c8e0233d0f92993debbf259908f52a4c4
dinujayadisanayaka <dmnddisanayaka@gmail.com>
2021-05-19 11:48:25 +0530

    ShopERR

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -92,7 +92,7 @@ class ShopController extends Controller
             $file=$request->file('avatar');
             $extension=$file->getClientOriginalExtension();//get image extension
             $filename= time().'.'.$extension;
-            $file->move('uploads/shops',$filename);
+            $file->move('uploads/shop',$filename);
             $Shop->image=$filename;
         }else{
             return $request;
