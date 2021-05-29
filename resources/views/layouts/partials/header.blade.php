6de3e502a3fa030c752f84cff29a349117d2a75a
dinujayadisanayaka <dmnddisanayaka@gmail.com>
2021-05-29 12:23:17 +0530

    orderfinish

--- resources/views/layouts/partials/header.blade.php
+++ resources/views/layouts/partials/header.blade.php
@@ -22,7 +22,7 @@
         <!-- Notifications Dropdown Menu -->
         <li class="nav-item dropdown">
             <a class="nav-link" data-toggle="dropdown" href="/">
-              Dinujaya Disanayaka
+              Bhagya Aththanayaka
 
             </a>
             <div class="dropdown-menu dropdown-menu-sm dropdown-menu-right">
