e5cb6df161971a450ee80257a2fb98e770795ee0
unknown <dmnddisanayaka@gmail.com>
2021-06-20 20:12:08 +0530

    MsgE2

--- database/migrations/2021_04_12_100336_create_messages_table.php
+++ database/migrations/2021_04_12_100336_create_messages_table.php
@@ -15,8 +15,8 @@ class CreateMessagesTable extends Migration
     {
         Schema::create('messages', function (Blueprint $table) {
             $table->id();
-//            $table->bigInteger('from');
-//            $table->bigInteger('to');
+            $table->text('from');
+            $table->text('to');
             $table->text('message');
             $table->tinyInteger('is_read');
         });
