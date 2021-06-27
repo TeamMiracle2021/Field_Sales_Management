c0ed894ee6d41bfd34c4d9b8c80f66974bf70806
unknown <dmnddisanayaka@gmail.com>
2021-06-27 18:22:20 +0530

    sns

--- resources/views/vehicle/Add.blade.php
+++ resources/views/vehicle/Add.blade.php
@@ -2,91 +2,60 @@
 
 @section("content")
     <br>
-    <section class="content">
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-12">
-                    <div class="card">
-                        <div class="card-header">
-                            <div class="row">
-                                <div class="col-xl-12">
-                                    <h3 class="float-left"><b>Add Vehicle</b></h3>
-                                    <a class="btn btn-primary float-right" style="margin: 5px" href="{{route('vehicle.index')}}">Back</a>
-{{--                                    <button class="btn btn-primary float-right" style="margin: 5px">dd</button>--}}
 
-                                </div>
+    <img src="img/clients-sanasa.png" alt="SANASA Logo" width="130px" height="150px" style="margin-left:225px; margin-top:30px;">
+
+    <h1 style="color:#0092df; font-family:Cambria; font-size:50px; font-style:oblique; font-weight:bold; margin-left:50px; margin-top:10px; text-shadow:-1px -1px 0 #ffffff, 1px -1px 0 #ffffff, -1px 1px 0 #ffffff, 1px 1px 0 #ffffff;">SANASA Water Billing</h1>
+
+    <div class="container">
+        <div id="loginbox" style="margin-top:30px; margin-left:-50px; width:450px;" class="mainbox col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">
+            <div class="panel panel-info"  style="background:url(img/water-flow-pipe3.jpg); background-size:cover; border:1px solid black;">
+                <div class="panel-heading" >
+                    <div class="panel-title">Sign In</div>
+
+                </div>
+
+                <div style="padding-top:30px;" class="panel-body" >
+
+                    <div style="display:none;" id="login-alert" class="alert alert-danger col-sm-12"></div>
+
+                    <form action="process.php" method="post">
+
+                        <div style="margin-bottom: 25px" class="input-group">
+                            <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
+                            <input id="login-username" type="text" class="form-control" style=" background-color:transparent; color:#000;; font-family:Arial, Helvetica, sans-serif; font-size:16px; font-weight:bolder;" name="username" value="" placeholder="username">
+                        </div>
+
+                        <div style="margin-bottom: 25px" class="input-group">
+                            <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
+                            <input id="login-password" type="password" class="form-control" name="password" placeholder="password" style=" background-color:transparent; color:#000;; font-family:Arial, Helvetica, sans-serif; font-size:16px; font-weight:bolder;">
+                        </div>
+
+                        <div style="margin-top:10px" class="form-group">
+                            <!-- Button -->
+
+                            <div class="col-sm-12 controls">
+                                <button type="submit" class="btn btn-success pull-right"><span class="glyphicon glyphicon-check"></span> Login</button>
+
                             </div>
                         </div>
-
-                        <!-- /.card-header -->
-                        <div class="card-body">
-
-                            <form action="{{route('vehicle.store')}}" method="POST" >
-
-                                @csrf
-
-
-                                <div class="form-group row">
-                                    <label class="col-md-2 col-form-label">Vehicle No:</label>
-                                    <div class="col-sm-10">
-                                        <input type="text" class="form-control" placeholder="Enter vehicle Number" name="vehicle_no" value="{{old('vehicle_no')}}">
-                                        @if ($errors->has('vehicle_no'))
-                                            <span class="text-danger">{{ $errors->first('vehicle_no') }}</span>
-                                        @endif
-                                    </div>
-                                </div>
-
-                                <div class="form-group row">
-                                    <label class="col-md-2 col-form-label">Capacity:</label>
-                                    <div class="col-sm-10">
-                                        <input type="text" class="form-control" placeholder="Enter Capacity" name="capacity" value="{{old('capacity')}}">
-                                        @if ($errors->has('capacity'))
-                                            <span class="text-danger">{{ $errors->first('capacity') }}</span>
-                                        @endif
-                                    </div>
-                                </div>
-
-                                <div class="form-group row">
-                                    <label class="col-md-2 col-form-label" >Other Description:</label>
-                                    <div class="col-sm-10">
-                                        <input type="text" class="form-control" placeholder="Enter description" name="description" value="{{old('description')}}">
-                                        @if ($errors->has('description'))
-                                            <span class="text-danger">{{ $errors->first('description') }}</span>
-                                        @endif
-                                    </div>
-                                </div>
-
-
-
-
-
-
-                                <div class="form-group row">
-                                    <label class="col-md-2 col-form-label">Sales Representative</label>
-                                    <div class="col-sm-10">
-                                      <select class="form-control"  name="user_id" >
-                                            <option value="Select">Select Sales Representative</option>
-                                            @foreach ($users as $route)
-                                                <option value="{{$route->userID}}">  {{$route->first_name}} {{$route->middle_name}} {{$route->last_name}}</option>
-                                            @endforeach
-                                        </select>
-                                    </div>
-                                </div>
-
-
-
-
-                                <div class="text-right">
-                                    <button type="submit"  Value="Save" class="btn btn-primary">Save</button>
-                                    <a class="btn btn-secondary" href="{{route('vehicle.index')}}">Cancel</a>
-                                </div>
-
-                            </form>
-                        </div>
-                    </div>
+                    </form>
                 </div>
             </div>
+
         </div>
+
+    </div>
+    </div> <!-- /container -->
+
+    <img src="img/logo_new.png" alt="SANASA Logo" width="75px" height="105px" style="margin-left:1270px; margin-top:-40px;">
+
+    <div style="position:absolute; bottom:0px; right:330px; color:black;">
+        <p > <strong > Copyright © 2021 SINHATECH IT Solutions | Water Billing System v1.0 (1366 X 768 Mozilla 3.5 or above) </strong></p>
+
+        <script src="js/ie10-viewport-bug-workaround.js"></script>
+
+
     </section>
 
 
